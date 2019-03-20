4f1edf0a2ad9a4beefd57621de98611f12342cca
Iskra B <ibatistapoblete@gmail.com>
2019-03-20 15:51:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 5 && 3 == 0
+    return "fizzbuzz"
+  else int !% 5 || 3
+    return "nil"
+  end
 end
